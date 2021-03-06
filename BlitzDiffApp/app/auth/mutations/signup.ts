6d5d5cf0cbfcc181d5e844e2dbb2ed32e0cf3bde
Matthieu PETIT <matthieu.petit@grenoble-inp.org>
2021-03-06 02:10:19 +0100

    Release 0.30.0-canary.5

---
+++ BlitzDiffApp/app/auth/mutations/signup.ts
@@ -0,0 +1,14 @@
+import { resolver, SecurePassword } from "blitz"
+import db from "db"
+import { Signup } from "app/auth/validations"
+
+export default resolver.pipe(resolver.zod(Signup), async ({ email, password }, ctx) => {
+  const hashedPassword = await SecurePassword.hash(password)
+  const user = await db.user.create({
+    data: { email: email.toLowerCase(), hashedPassword, role: "user" },
+    select: { id: true, name: true, email: true, role: true },
+  })
+
+  await ctx.session.$create({ userId: user.id, roles: [user.role] })
+  return user
+})
