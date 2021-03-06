7da899d43f82b53b3da5dbced838b8739338adc9
Matthieu PETIT <matthieu.petit@grenoble-inp.org>
2021-03-06 01:00:15 +0100

    remove "

--- expect_new_release.sh
+++ expect_new_release.sh
@@ -2,7 +2,7 @@
     set timeout -1
     set version [lindex $argv 0]
     set appname [lindex $argv 1]
-    spawn npx blitz@"$version" new "$appname" --no-git
+    spawn npx blitz@$version new $appname --no-git
     expect "No - Continue with old version"
     send -- "\[B"
     expect "Your global blitz version is outdated. Upgrade?"
