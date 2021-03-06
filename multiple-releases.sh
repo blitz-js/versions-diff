0469cac4485fd67b41bbacc328f4715eac2685b2
Matthieu PETIT <matthieu.petit@grenoble-inp.org>
2021-03-06 02:21:49 +0100

    Add release 0.29.7

--- multiple-releases.sh
+++ multiple-releases.sh
@@ -3,20 +3,6 @@ set -euxo pipefail
 
 
 releases=(
-0.31.0
-0.30.7
-0.30.6
-0.30.0-canary.11
-0.30.0-canary.10
-0.30.0-canary.9
-0.30.0-canary.8
-0.30.0-canary.7
-0.30.0-canary.6
-0.30.0-canary.5
-0.30.0-canary.4
-0.30.0-canary.3
-0.30.0-canary.2
-0.30.0-canary.1
 0.29.7
 0.29.6
 0.29.5
