a7625b43eee4c03cc760a38da10c7e1d1723d869
Matthieu PETIT <matthieu.petit@grenoble-inp.org>
2021-03-06 01:29:31 +0100

    Add release 0.30.0-canary.12

--- multiple-releases.sh
+++ multiple-releases.sh
@@ -19,6 +19,20 @@ releases=(
 0.30.0-canary.2
 0.30.0-canary.1
 0.30.0-canary.0
+0.29.7
+0.29.6
+0.29.5
+0.29.4
+v0.29.3
+v0.29.2
+v0.29.1
+v0.29.0
+v0.29.0-canary.0
+v0.28.0
+v0.28.0-canary.2
+v0.28.0-canary.1
+v0.28.0-canary.0
+v0.27.1
 )
 
 for v in "${releases[@]}"
